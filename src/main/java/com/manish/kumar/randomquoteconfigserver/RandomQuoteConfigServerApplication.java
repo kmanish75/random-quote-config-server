bc3529f0e14e15d87b6040a81a1bb0945076693e
Manish <kmanish75@gmail.com>
2019-08-19 10:41:24 -0500

    random quote commits

---
+++ src/main/java/com/manish/kumar/randomquoteconfigserver/RandomQuoteConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.manish.kumar.randomquoteconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQuoteConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomQuoteConfigServerApplication.class, args);
+	}
+
+}
